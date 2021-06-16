ed854737be80c671755cae837b9aba98925f76bd
TenzingS <thesherpatenzing@gmail.com>
2021-06-16 10:12:13 -0400

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  let taskInputForm = document.getElementById('create-task-form')
+  let newTaskInputBox = document.getElementById('new-task-description')
+  let taskHolderBox = document.getElementById('tasks')
+
+  taskInputForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let taskList = document.createElement('li');
+    let newTask = newTaskInputBox.value;
+    taskList.innerText = newTask;
+    taskHolderBox.append(taskList);
+  }
+  )
+}
+)
+
